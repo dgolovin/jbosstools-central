e2d4988536550c66ce66087d44275c7a51cf50ff
Snjezana Peco <snjezana.peco@redhat.com>
2013-03-13 20:00:18 +0100

    JBIDE-13599 - externalize Central site URL

---
+++ central/plugins/org.jboss.tools.central/src/org/jboss/tools/central/internal/discovery/DiscoveryConnector.java
@@ -0,0 +1,36 @@
+/*************************************************************************************
+ * Copyright (c) 2008-2013 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     JBoss by Red Hat - Initial implementation.
+ ************************************************************************************/
+package org.jboss.tools.central.internal.discovery;
+
+import org.eclipse.mylyn.internal.discovery.core.model.ValidationException;
+import org.jboss.tools.central.JBossCentralActivator;
+import org.jboss.tools.central.internal.xpl.ExpressionResolver;
+
+/**
+ * 
+ * @author snjeza
+ *
+ */
+public class DiscoveryConnector extends org.eclipse.mylyn.internal.discovery.core.model.DiscoveryConnector {
+
+	@Override
+	public void validate() throws ValidationException {
+		if (siteUrl != null) {
+			try {
+				siteUrl = ExpressionResolver.DEFAULT_RESOLVER.resolve(siteUrl);
+			} catch (Exception e) {
+				JBossCentralActivator.log(e);
+			}
+		}
+		super.validate();
+	}
+
+}
